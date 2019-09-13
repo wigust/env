d2e93d5bed22f72a994e60ee2051835599297010
Ben Buscarino <buscari3@msu.edu>
2019-09-13 01:50:04 -0400

    Whoops

--- configuration.nix
+++ configuration.nix
@@ -11,90 +11,115 @@ in
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      # Also include binary caches managed by cachix
       /etc/nixos/cachix.nix
+      "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
     ];
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
 
   networking.hostName = "busc-nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
+  # Set the time zone.
+  time.timeZone = "America/Detroit";
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git zsh wget curl emacs chromium cachix sqlite
+    git oh-my-zsh wget curl
+
+    google-chrome chromium vlc epdfview steam konsole
+
+    cachix sqlite gparted ark home-manager exa
+
+    python3 pipenv python37Packages.virtualenv
+
+    emacs jetbrains.clion jetbrains.datagrip jetbrains.idea-ultimate vscode
+
+    jetbrains.jdk bazel gradle
+
+    cmake gnumake clang
+    stack ghc stack2nix cabal-install cabal2nix
+
     (all-hies.selection { selector = p: { inherit (p) ghc865; }; })
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
+  fonts.enableFontDir = true;
+  fonts.enableDefaultFonts = true;
+  fonts.enableCoreFonts = true;
+  fonts.fonts = with pkgs; [
+    corefonts
+    font-awesome-ttf
+    fira-code
+  ];
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
 
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.videoDrivers = [ "nvidia" ];
-  # services.xserver.xkbOptions = "eurosign:e";
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
+    videoDrivers = [ "nvidia" ];
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.default = "plasma5";
-  services.xserver.desktopManager.plasma5.enable = true;
-  # services.xserver.displayManager.sddm.autoLogin.enable = true;
-  # services.xserver.displayManager.sddm.autoLogin.user = "ben";
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+        feh
+        arandr
+        pavucontrol
+        breeze-gtk
+     ];
+    };
+
+  };
   users.users.ben = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "wheel"
+      "docker"
+    ]; # Enable ‘sudo’ for the user.
+    initialPassword = "1234";
   };
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
+
+  networking.extraHosts =
+    ''
+      192.168.86.38 desktop.busc
+      192.168.86.235 laptop.busc
+    '';
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.03"; # Did you read the comment?
-
 }
