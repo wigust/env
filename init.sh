f66dc1df60031116b17251dbfce6e6a6712b54a1
Ben Buscarino <buscari3@msu.edu>
2019-09-18 03:17:07 -0400

    Minor change to init script

--- init.sh
+++ init.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # Installs all the dotfiles and such with home-manager
 
+
+print() {
+    echo -e "\e[32;1m$1\e[0m"
+}
+
+ensure_installed() {
+    if ! hash $1 2>/dev/null; then
+        print "Installing $1\..."
+        nix-env -e $1
+    fi
+}
+
 if ! hash nix 2>/dev/null; then
-    echo "Installing nix..."
+    print "Installing nix..."
     curl https://nixos.org/nix/install | sh
 fi
-echo "Installing git..."
-nix-env -e git
-echo "Cloning env..."
-git clone https://github.com/bbuscarino/env.git $HOME\/env
-nix-env -e home-manager
+ensure_installed git
+print "Cloning env..."
+git clone git@github.com:bbuscarino/env.git $HOME\/env
+ensure_installed home-manager
+print "Activating home..."
 home-manager -f $HOME\/env/home.nix switch
-echo "Done!"
+print "Done!"
 exit 0
