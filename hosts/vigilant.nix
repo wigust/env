aa5d94989601695be728b0bbb5cb03e6755ee261
Ben Buscarino <buscari3@msu.edu>
2021-01-04 17:20:40 -0500

    Integrate vigilant hardware stuff

--- hosts/vigilant.nix
+++ hosts/vigilant.nix
@@ -8,30 +8,27 @@
 
   boot = {
     loader = {
-      grub = {
-        enable = true;
-        version = 2;
-        device = "/dev/nvme0n1";
-      };
+      systemd-boot.enable = true;
+      efi.canTouchEFIVariables = true;
     };
     initrd.availableKernelModules =
-      [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" "exfat" ];
+      [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
     kernelModules = [ "kvm-amd" ];
     supportedFilesystems = [ ];
   };
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/cc55a8e5-9b2f-4562-bdf8-5efdaa1a7f68";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/b92f846d-ef46-497c-96b8-50082e84e282";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/AE33-365C";
+      fsType = "vfat";
+    };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/2bbdeb41-6a64-4341-a04f-a3f01cbe812a"; }];
-
-  fileSystems."/mnt/windows" = {
-    device = "/dev/disk/by-uuid/4E3604C83604B34F";
-    fsType = "ntfs";
-    options = [ "rw" "uid=${toString config.users.users.ben.uid}" ];
-  };
+    [ { device = "/dev/disk/by-uuid/4eed79ae-0676-4f69-a27e-1f503993869b"; }
+    ];
 
   nix.maxJobs = lib.mkDefault 16;
   environment.systemPackages = with pkgs; [ mesa ];
--- profiles/network/default.nix
+++ profiles/network/default.nix
@@ -1,8 +1,8 @@
 {
   imports = [
-    #./networkmanager
+    ./networkmanager
+    ./tailscale.nix
     # ./adblocking.nix
     # ./stubby.nix
-    ./tailscale.nix
   ];
 }
