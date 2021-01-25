d4a307435ec50ac576c18cf6bbd3c8f416e6639a
Ben Buscarino <buscari3@msu.edu>
2021-01-24 19:34:44 -0500

    Remove capslock

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -64,6 +64,12 @@ in
 
   services.xserver = {
     enable = true;
+
+    displayManager.sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap ${pkgs.writeText "xkb-layout" ''
+      ! disable capslock
+      remove Lock = Caps_Lock
+    ''}";
+
     displayManager.lightdm.greeters.gtk = {
       enable = true;
       theme = {
