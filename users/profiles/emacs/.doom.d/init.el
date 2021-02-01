c9db1f9edea1b585fb420f0ff8607d824d2cd709
Ben Buscarino <bbuscarino@morsecorp.com>
2021-01-31 20:50:33 -0500

    doom changes

--- users/profiles/emacs/.doom.d/init.el
+++ users/profiles/emacs/.doom.d/init.el
@@ -1,79 +1,77 @@
 (doom! 
-       :ui
-       doom
-       doom-dashboard
-       (emoji +unicode +ascii +github)
-       modeline
-       ophints
-       minimap
-       (ligatures +extra +fira)
-       hl-todo
-       workspaces
-       (window-select +numbers)
-       vi-tilde-fringe
-       vc-gutter
-       tabs
-       (popups +defaults)
-       treemacs
-       nav-flash
+ :ui
+ doom
+ doom-dashboard
+ (emoji +unicode +ascii +github)
+ modeline
+ ophints
+ minimap
+ (ligatures +extra +fira)
+ hl-todo
+ workspaces
+ (window-select +numbers)
+ vi-tilde-fringe
+ vc-gutter
+ tabs
+ (popups +defaults)
+ treemacs
+ nav-flash
 
-       :config
-       (default +bindings)
+ :config
+ (default +bindings +smartparens)
 
-       :editor
-       evil
-       fold
-       (format +onsave)
-       parinfer
-       snippets
+ :editor
+ evil
+ fold
+ format
+ parinfer
+ snippets
 
-       :emacs
-       (dired +ranger +icons)
-       (ibuffer +icons)
-       (undo +tree)
-       vc
+ :emacs
+ (dired +ranger +icons)
+ (ibuffer +icons)
+ (undo +tree)
+ vc
 
-       :completion
-       (company +childframe)
-       helm
+ :completion
+ (company +childframe)
+ helm
 
-       :checkers
-       spell
-       syntax
+ :checkers
+ spell
+ syntax
 
-       :term
-       shell
-       term
-       vshell
+ :term
+ shell
+ term
+ vshell
 
-       :tools
-       debugger
-       direnv
-       (docker +lsp)
-       (eval +overlay)
-       (lookup +docsets)
-       (lsp +peek)
-       (magit +forge)
-       prodigy
-       terraform
-       
-       :os
-       (:if IS-MAC macos)
+ :tools
+ debugger
+ direnv
+ (docker +lsp)
+ (eval +overlay)
+ (lookup +docsets)
+ (lsp +peek)
+ (magit +forge)
+ prodigy
+ terraform
 
-       :email
+ :os
+ (:if IS-MAC macos)
 
-       :lang
-       (python +lsp +pyright +pyenv +poetry +cython)
-       json
-       data  ;; CSV, etc
-       nix
-       emacs-lisp
-       (org +brain +pandoc +present +pretty +roam)
-       web
-       (markdown +grip)
-       (javascript +lsp)
-       hy
-       yaml
-       (purescript +lsp)
-       (sh +lsp)
-       (haskell +lsp))
+ :lang
+ (python +lsp +pyright +pyenv +poetry +cython)
+ json
+ data  ;; CSV, etc
+ nix
+ emacs-lisp
+ (org +brain +pandoc +present +pretty +roam)
+ web
+ (markdown +grip)
+ (javascript +lsp)
+ hy
+ yaml
+ (purescript +lsp)
+ (sh +lsp)
+ (haskell +lsp))
